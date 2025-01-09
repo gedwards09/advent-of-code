ecdbe12d7fb12176c91963e7347a67ed424208ee
gedwards09 <greg.edwards09@gmail.com>
2025-01-08 22:15:13 -0600

    Create Int2DArray and update day-1.py

--- 2024/day-1.py
+++ 2024/day-1.py
@@ -1,28 +1,13 @@
 import sys
-from utils.AocArgs import AocArgs
-from utils.AocTester import AocTester
+from utils.AocController import AocController
+from utils.Int2DArray import Int2DArray
 
-def alg(args):
-    if args.PartNumber == 1:
-        return alg_1(args.InputFileName)
-    elif args.PartNumber == 2:
-        return alg_2(args.InputFileName)
-
-def alg_1(fileName):
-    arr = parseFile(fileName)
-    arr = transpose(arr)
-    arr[0].sort()
-    arr[1].sort()
-    return getDistance(arr[0], arr[1])
-
-def parseFile(fileName):
-    with open(fileName,'r') as f:
-        return [[int(el) for el in line.split()]\
-                for line in f.read().split('\n')]
-
-def transpose(arr):
-        return [[int(arr[rowNum][colNum]) for rowNum in range(len(arr))]\
-                for colNum in range(len(arr[0]))]
+def Alg1(sFileName):
+    arr = Int2DArray(sFileName)
+    arr.Transpose()
+    arr.getRow(0).sort()
+    arr.getRow(1).sort()
+    return getDistance(arr.getRow(0), arr.getRow(1))
     
 def getDistance(arr1, arr2):
     if len(arr1) != len(arr2):
@@ -32,28 +17,27 @@ def getDistance(arr1, arr2):
         dist += abs(arr2[colNum] - arr1[colNum])
     return dist
 
-def alg_2(fileName):
-    arr = parseFile(fileName)
+def Alg2(sFileName):
+    arr = Int2DArray(sFileName)
     dic = {}
     colNum = 1
-    for rowNum in range(len(arr)):
-        val = arr[rowNum][colNum]
+    for rowNum in range(arr.getYLength()):
+        val = arr.get(rowNum, colNum)
         if val in dic:
             dic[val] += 1
         else:
             dic[val] = 1
     sum = 0
     colNum = 0
-    for rowNum in range(len(arr)):
-        val = arr[rowNum][colNum]
+    for rowNum in range(arr.getYLength()):
+        val = arr.get(rowNum, colNum)
         if val in dic:
             sum += val * dic[val]
     return sum
 
 def __main__():
-    args = AocArgs(sys.argv)
-    value = alg(args)
-    AocTester.test(args, value)
+    c = AocController(sys.argv, Alg1, Alg2)
+    c.Execute()
 
 if __name__ == "__main__":
     __main__()
