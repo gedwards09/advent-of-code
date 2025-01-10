4aedac196d2e9e6ef00e30b61b47fe09101aaf75
gedwards09 <greg.edwards09@gmail.com>
2025-01-09 19:42:59 -0600

    Add template file

---
+++ 2024/TEMPLATE.py
@@ -0,0 +1,15 @@
+import sys
+from utils.AocController import AocController
+
+def Alg1(sFileName):
+    return
+
+def Alg2(sFileName):
+    return
+
+def __main__():
+    c = AocController(sys.argv, Alg1, Alg2)
+    c.Execute()
+
+if __name__ == "__main__":
+    __main__()
